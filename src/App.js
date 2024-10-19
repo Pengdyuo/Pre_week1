a8305e71d0d475c794b6fce6fece08075385215b
Minho Kim <wweer4748@naver.com>
2024-10-20 05:28:15 +0900

    커스텀 구분자 지원

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ class App {
       const input = await this.getInput();
       const result = this.calculateSum(input);
       this.printResult(result);
-    }
-    catch (error) {
+    } catch (error) {
       Console.print(error.message);
     }
   }
 
   async getInput() {
     return new Promise((resolve) => {
-      Console.readLineAsync("덧셈할 문자열을 입력해 주세요: ", (input) => { resolve(input); });
+      Console.readLineAsync("덧셈할 문자열을 입력해 주세요: ", (input) => {
+        resolve(input);
+      });
     });
   }
 
@@ -23,8 +24,19 @@ class App {
       return 0;
     }
 
-    const delimiters = /[,:]/;
-    const numbers = input.split(delimiters).map(Number);
+    let delimiters = /[,:]/;
+    let numbersString = input;
+
+    // 커스텀 구분자 처리
+    if (input.startsWith("//")) {
+      const customDelimiterMatch = input.match(/^/ / (.) \n(.*) /);
+      if (customDelimiterMatch) {
+        delimiters = new RegExp(customDelimiterMatch[1]);
+        numbersString = customDelimiterMatch[2];
+      }
+    }
+
+    const numbers = numbersString.split(delimiters).map(Number);
 
     if (numbers.some((num) => num < 0)) {
       throw new Error("[ERROR] 음수는 허용되지 않습니다.");
