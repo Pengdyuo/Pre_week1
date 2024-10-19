64bec02f2e2979accb12535c100ed7c4a0ea7b34
Minho Kim <wweer4748@naver.com>
2024-10-20 05:24:06 +0900

    기본 입력 구분자 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await this.getInput();
+      const result = this.calculateSum(input);
+      this.printResult(result);
+    }
+    catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  async getInput() {
+    return new Promise((resolve) => {
+      Console.readLineAsync("덧셈할 문자열을 입력해 주세요: ", (input) => { resolve(input); });
+    });
+  }
+
+  calculateSum(input) {
+    if (!input) {
+      return 0;
+    }
+
+    const delimiters = /[,:]/;
+    const numbers = input.split(delimiters).map(Number);
+
+    if (numbers.some((num) => num < 0)) {
+      throw new Error("[ERROR] 음수는 허용되지 않습니다.");
+    }
+
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
+
+  printResult(result) {
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
