0e8bb1a282226590b92d401441cfb3b0f1cd38d2
Minho Kim <wweer4748@naver.com>
2024-10-20 19:11:11 +0900

    구분된 숫자 포함된 문자열 받기

--- src/App.js
+++ src/App.js
@@ -7,49 +7,21 @@ class App {
   run() {
     return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
       .then((input) => {
-        const result = this.calculate(input);
-        Console.print(`결과 : ${result}`);
+        const numbers = this.parseNumbers(input);
+        Console.print(입력된 숫자들 : ${ numbers });
       })
       .catch((error) => {
         Console.print(error.message);
-        throw error; // 예외를 다시 던져 테스트에서 감지할 수 있도록 합니다.
+        throw error;
       });
-    // Console.close()를 제거했습니다.
-  }
-
-  calculate(input) {
-    if (input === '' || input === null) return 0;
-
-    const numbers = this.parseNumbers(input);
-    this.validateNumbers(numbers);
-
-    return numbers.reduce((sum, num) => sum + num, 0);
   }
 
   parseNumbers(input) {
     let delimiters = [',', ':'];
     let numbersString = input;
 
-    const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)/);
-    if (customDelimiterMatch) {
-      delimiters.push(customDelimiterMatch[1]);
-      numbersString = customDelimiterMatch[2];
-    }
-
-    // 구분자를 정규식에서 안전하게 사용하도록 이스케이프 처리
-    const escapedDelimiters = delimiters.map((d) => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'));
-    const regex = new RegExp(escapedDelimiters.join('|'));
-
-    return numbersString.split(regex).map((num) => parseInt(num, 10));
-  }
-
-  validateNumbers(numbers) {
-    numbers.forEach((num) => {
-      if (isNaN(num) || num < 0) {
-        throw new Error('[ERROR] 올바른 양수를 입력해 주세요.');
-      }
-    });
   }
 }
 
 export default App;
+
