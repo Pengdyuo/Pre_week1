0b31a6972c4de8203deea11d3d68771560928808
Minho Kim <wweer4748@naver.com>
2024-10-20 19:29:08 +0900

    양수만 처리

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
     return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
       .then((input) => {
         const result = this.calculate(input);
-        Console.print(`결과 : ${result}`);
+        Console.print(결과 : ${ result });
       })
       .catch((error) => {
         Console.print(error.message);
@@ -25,16 +25,6 @@ class App {
     let delimiters = [',', ':'];
     let numbersString = input;
 
-    const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)/);
-    if (customDelimiterMatch) {
-      delimiters.push(customDelimiterMatch[1]);
-      numbersString = customDelimiterMatch[2];
-    }
-
-    const escapedDelimiters = delimiters.map((d) => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'));
-    const regex = new RegExp(escapedDelimiters.join('|'));
-
-    return numbersString.split(regex).map((num) => parseInt(num, 10));
   }
 }
 
