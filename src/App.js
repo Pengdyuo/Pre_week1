38c1ed31d0f0baa4f1cecbda820a7b89711ae987
Minho Kim <wweer4748@naver.com>
2024-10-20 19:29:56 +0900

    오류 메세지 표시

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
     return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
       .then((input) => {
         const result = this.calculate(input);
-        Console.print(결과 : ${ result });
+        Console.print(`결과 : ${result}`);
       })
       .catch((error) => {
         Console.print(error.message);
@@ -25,6 +25,22 @@ class App {
     let delimiters = [',', ':'];
     let numbersString = input;
 
+    const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)/);
+    if (customDelimiterMatch) {
+      delimiters.push(customDelimiterMatch[1]);
+      numbersString = customDelimiterMatch[2];
+    }
+
+    const escapedDelimiters = delimiters.map((d) => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'));
+    const regex = new RegExp(escapedDelimiters.join('|'));
+
+    return numbersString.split(regex).map((num) => {
+      const parsedNum = parseInt(num, 10);
+      if (isNaN(parsedNum) || parsedNum < 0) {
+        throw new Error('[ERROR] 올바른 양수를 입력해 주세요.');
+      }
+      return parsedNum;
+    });
   }
 }
 
